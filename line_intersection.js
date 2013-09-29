341a4c95c09c2fae4fb44fd8448f402b0b3ff1dd
Vigneshwaran Raveendran <vigneshwaran2007@gmail.com>
2013-09-29 15:01:40 +0530

    Update line_intersection.js

--- line_intersection.js
+++ line_intersection.js
@@ -100,7 +100,7 @@ function getLineIntersectionData(line1_data, line2_data, user_options) {
     // [[x1, y1], [x2, y2], ... [xn, yn]]
     var _tmp_arr = [line1_data, line2_data, line1_data[0], line2_data[0]];
     for (var i = 0; i < _tmp_arr.length; i++) {
-        if (Object.prototype.toString.call(_tmp_arr[i]) !== "[object Array]") {
+        if (_tmp_arr[i] == null || Object.prototype.toString.call(_tmp_arr[i]) !== "[object Array]") {
             console.error('The first two arguments should each be of ' +
                           'the form: [[x1, y1], [x2, y2], ... [xn, yn]]');
             return;
